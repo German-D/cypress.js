7629c14a5c45c243926f5978cb07deb7ecaeda55
German Dolnikov <german@dolnikov.ru>
2022-05-04 23:56:39 +0300

    Update google.js

--- cypress/integration/google.js
+++ cypress/integration/google.js
@@ -1,6 +1,6 @@
 
-describe('Главная гугла. Проверка, что в выдаче есть тесла', function () {
-   it('search something', function () {
+describe('Тестирование главной гугла', function () {
+   it('Проверка, что при поиске теслы в выдаче есть тесла', function () {
         cy.visit('https://google.com');
         cy.get("input[type='text']").type('tesla').type('{enter}');
         cy.contains('https://www.tesla.com');
