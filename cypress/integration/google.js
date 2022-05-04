9c01e747c9750be930128d3ed03aa6ea451f0f9a
German Dolnikov <german@dolnikov.ru>
2022-05-04 23:33:48 +0300

    Update google.js

--- cypress/integration/google.js
+++ cypress/integration/google.js
@@ -1,15 +1,8 @@
 
 describe('Главная гугла. Проверка, что в выдаче есть тесла', function () {
-    
-    it('search something', function () {
-        cy.visit('https://staya.dog/');
-        cy.contains('input', 'Поводки').click();
-        // cy.url().should('eq', 'https://staya.dog/catalog/dogs/lsh')
-        cy.contains('Heatwave');
-        cy.end();
-         })
-
-     it('search something', function () {
-        cy.visit('https://staya.dog/');
-        })
+   it('search something', function () {
+        cy.visit('https://google.com');
+        cy.get("input[type='text']").type('tesla').type('{enter}');
+        cy.contains('https://www.tesla.com');
+    })
 })
