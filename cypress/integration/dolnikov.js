2f87203d948c5d5ba3f39e4567ddbb6115c55c98
German Dolnikov <german@dolnikov.ru>
2022-05-05 01:34:13 +0300

    Delete cypress/integration directory

--- cypress/integration/dolnikov.js
+++
@@ -1,9 +0,0 @@
-
-describe('Тестирование dolnikov.ru', function () {
-
-    it('Проверка, что при переходе в статью урл будет верный', function () {
-        cy.visit('https://dolnikov.ru/blog/');
-        cy.contains('Про образование').click();
-        cy.url().should('eq', 'https://dolnikov.ru/blog/all/pro-obrazovanie/')
-    })
-})
--- cypress/integration/google.js
+++
@@ -1,8 +0,0 @@
-
-describe('Тестирование главной гугла', function () {
-   it('Проверка, что при поиске теслы в выдаче есть тесла', function () {
-        cy.visit('https://google.com');
-        cy.get("input[type='text']").type('tesla').type('{enter}');
-        cy.contains('https://www.tesla.com');
-    })
-})
